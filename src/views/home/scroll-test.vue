ae743e3db3af97099214ae2f1288627facefd77c
xxx <2624823814@qq.com>
2021-04-13 14:28:27 +0800

    init

--- src/views/home/scroll-test.vue
+++ src/views/home/scroll-test.vue
@@ -3,7 +3,7 @@
   <div style="padding: 50px;background-color:#fff;">
     <div>下方是组件</div>
     <div style="border: 1px solid #ddd">
-      <scroll-container :step="1000" width="200px" height="200px">
+      <scroll-container :step="10" width="200px" height="200px">
         <div v-for="i in 20" :key="i">
           {{i}}哇哈哈{{i}}
         </div>
